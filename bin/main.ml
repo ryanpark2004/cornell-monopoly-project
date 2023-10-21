43b1f93b89aa4148e7daabb6cc206fc405be0c7a
Ethan Baker <eeb73@cornell.edu>
2023-10-21 16:00:58 -0400

    Fixed print_board and other basic print functions to create a clearer dialogue.

--- bin/main.ml
+++ bin/main.ml
@@ -12,14 +12,14 @@ type state = {
 }
 
 let new_state : state =
-  { board = Board.new_board; players = []; max_run = 10; current_run = 0 }
+  { board = Board.new_board; players = []; max_run = 9; current_run = 0 }
 
 (**[add_players] is a new state with new players added. 
     The number of players must be less than or equal to 4,
      or invalid argument exception is raised. The name of each 
      player must be nonempty and 10 character long or less*)
 let initialize_players (s : state) : state =
-  print_endline "How many players?: \n";
+  print_string "How many players? > ";
   let num_players = int_of_string (read_line ()) in
   if num_players <= 0 || num_players > 4 then invalid_arg "Too many players"
   else
@@ -27,7 +27,9 @@ let initialize_players (s : state) : state =
       match n with
       | 0 -> []
       | x ->
-          let () = print_endline ("Name of player " ^ string_of_int x ^ ":") in
+          let () =
+            print_string ("\nName of Player " ^ string_of_int x ^ " > ")
+          in
           let player_name = read_line () in
           let name_length = String.length player_name in
           if name_length <= 0 || name_length > 10 then
@@ -66,10 +68,10 @@ let rec pretty_board (plst : player list) (b : board) :
 
 let rec print_board (pb : (tile * player list) list) : unit =
   match pb with
-  | [] -> print_endline "------------------\n"
+  | [] -> print_endline "----------------------------\n"
   | (h1, h2) :: t ->
       let tile, plst = (h1, h2) in
-      print_string (to_string tile);
+      print_string ("| " ^ to_string tile);
       if not (h2 = []) then print_string " --- " else print_string "";
       let rec print_players name_list =
         match name_list with
@@ -80,10 +82,15 @@ let rec print_board (pb : (tile * player list) list) : unit =
             print_players t
       in
       print_players h2;
-      print_endline "\n";
+      print_endline "\n|";
       print_board t
 
-let print_state (s : state) = s.board |> pretty_board s.players |> print_board
+let print_board_frame pb =
+  print_endline "------- UPDATED BOARD ------- \n|";
+  print_board pb
+
+let print_state (s : state) =
+  s.board |> pretty_board s.players |> print_board_frame
 
 (******************************************************************************)
 
@@ -93,29 +100,26 @@ let print_state (s : state) = s.board |> pretty_board s.players |> print_board
     As of now, the action only contains changes in player position*)
 let action player : player =
   let n = rollDice () in
-  print_endline ("Rolled dice: " ^ string_of_int n);
+  print_endline ("You rolled a " ^ string_of_int n ^ "!\n");
   { name = "info"; money = 0; properties = []; position = n; in_jail = false }
 
 (**[update_player] is the new player whose fields have changed using [info]*)
 let update_player (p : player) (info : player) : player =
-  let p =
-    {
-      p with
-      money = p.money + info.money;
-      properties = p.properties @ info.properties;
-      position = (p.position + info.position) mod 6;
-      in_jail = info.in_jail;
-    }
-  in
-  print_int p.position;
-  p
+  {
+    p with
+    money = p.money + info.money;
+    properties = p.properties @ info.properties;
+    position = (p.position + info.position) mod 6;
+    in_jail = info.in_jail;
+  }
 
 (**[single_turn] is an updated state after every player performed an action *)
 let single_turn (s : state) : state =
   let rec helper (plst : player list) : player list =
     match plst with
     | [] ->
-        print_endline ("Turn #" ^ string_of_int s.current_run ^ " ended");
+        print_endline
+          ("Turn #" ^ string_of_int (s.current_run + 1) ^ " ended. \n");
         []
     | h :: t ->
         let change = action h in
@@ -129,7 +133,7 @@ let rec loop (s : state) (eval : state -> state) : unit =
   if s.current_run > s.max_run then
     print_endline "Max run reached. Thanks for playing!"
   else begin
-    print_string "Press Enter To Roll the Dice > ";
+    print_string "\nPress [ENTER] to roll the dice > ";
     let input = read_line () in
     match input with
     | "" ->
